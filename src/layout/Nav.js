74ad70b219e511dab897e042a14461f02fd8a50d
tiana30 <tiana3059@gmail.com>
2020-10-07 08:38:20 +0700

    nav hardcoded

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const Nav = () => (
     <nav className='bg-purple-600 p-4 text-white'>
     <ul className='flex space-x-4 justify-end'>
         <li>
-        <Link to="/">Home</Link>
+        <NavLink exact to="/" activeClassName="border-b-2 border-white">Home</NavLink>
         </li>
         <li>
-        <Link to="/about">About</Link>
+        <NavLink exact to="/about" activeClassName="border-b-2 border-white">About</NavLink>
         </li>
         <li>
-        <Link to="/contact">Contact</Link>
+        <NavLink exact to="/contact" activeClassName="border-b-2 border-white">Contact</NavLink>
         </li>
     </ul>
     </nav>
