2f989e0e4853f7f3bf4b772d2da52d47503cedb1
tiana30 <tiana3059@gmail.com>
2020-10-07 09:48:22 +0700

    add pages not found

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Home, About, Contact} from './pages';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import {Footer, Nav} from './layout';
+import NotFound from './pages/404';
 function App() {
   return (
     <div className='flex flex-col h-screen'>
@@ -14,9 +15,13 @@ function App() {
       <Route path='/about'>
         <About />
       </Route>
-      <Route exact path='/contact'>
+      <Route path='/contact'>
         <Contact />
       </Route>
+      <Route path='/404'>
+        <NotFound />
+      </Route>
+      <Redirect to='/404' />
       </Switch>
       <Footer />
     </Router>
---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Layout} from '../layout';
+
+const NotFound = () => (
+    <Layout>
+        <h1 className='text-2xl'>404</h1>
+        <p>Halaman ini tidak dapat ditemukan coba pakai halaman yang sudah disediakan</p>
+    </Layout>
+);
+
+export default NotFound;
