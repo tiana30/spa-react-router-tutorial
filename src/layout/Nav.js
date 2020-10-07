000f25b1901f780dddcd59b81e509944ff9d16a2
tiana30 <tiana3059@gmail.com>
2020-10-07 08:56:04 +0700

    use dynamic data

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
+const navs = [{
+    path:'/',
+    name:'Home'
+    },
+    {
+    path:'/about',
+    name:'About'
+    },
+    { 
+    path:'/contact',
+    name:'Contact'
+    } 
+]
 const Nav = () => (
     <nav className='bg-purple-600 p-4 text-white'>
     <ul className='flex space-x-4 justify-end'>
-        <li>
-        <NavLink exact to="/" activeClassName="border-b-2 border-white">Home</NavLink>
-        </li>
-        <li>
-        <NavLink exact to="/about" activeClassName="border-b-2 border-white">About</NavLink>
-        </li>
-        <li>
-        <NavLink exact to="/contact" activeClassName="border-b-2 border-white">Contact</NavLink>
-        </li>
+        {navs.map(navItem => (
+            <li>
+                <NavLink exact to={navItem.path}
+                activeClassName="border-b-2 border-white text-purple-100">
+                {navItem.name}
+                </NavLink>
+            </li>
+        ))}
     </ul>
     </nav>
 )
